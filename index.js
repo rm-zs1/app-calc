ab8fd821be52d37e2ece6bd56ba0481478058906
RM <rm.bochnia@gmail.com>
2024-02-07 08:58:53 +0100

    Added switch-case to index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const number1 = Number(prompt('Input 1 number'));
 const number2 = Number(prompt('Input 2 number'));
 const sign = Number(prompt('Input sign + - * /'));
+let result;
+switch (sign) {
+    case '+':result = number1 - number2; 
+    break;
+    case '-':result = number1 * number2; 
+    break;
+    case '*':result = number1 / number2; 
+    break;
+    case '/':result = number1 + number2; 
+    break;
+    default: 
+}
