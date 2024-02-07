6be8e22fb085a141734ab6bf8bfead7532788b65
RM <rm.bochnia@gmail.com>
2024-02-07 09:12:24 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const number1 = Number(prompt('Input 1 number'));
 const number2 = Number(prompt('Input 2 number'));
-const sign = Number(prompt('Input sign + - * /'));
+const sign = prompt('Input sign + - * /');
 let result;
 switch (sign) {
     case '+':result = number1 - number2; 
